6c0e07d0552af51d5c0b6f785901dc05713039da
Namaan <richardqwe46@gmail.com>
2023-06-13 00:11:11 +0900

    feat : Gimmick 충돌 수정
    - 'Hit' 이벤트가 아닌, 'OverlapMultiByObjectType'을 사용하여 다중 오버랩 감지
    - 클래스명 ‘SildeGimmick’ → ‘ObstacleMover’로 수정
    - 장애물 구현

---
+++ Source/PullAndPush/Private/Gimmick/InteractiveObstacleMover.cpp
@@ -0,0 +1,78 @@
+#include "Gimmick/InteractiveObstacleMover.h"
+#include "Components/CapsuleComponent.h"
+#include "Interface/CharacterInterActionHandler.h"
+
+AInteractiveObstacleMover::AInteractiveObstacleMover()
+	:
+	ForceStrength(2000.f), bLimitedTimeHit(0), MinHitTime(0.f), MaxHitTime(0.f)
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+	NetUpdateFrequency = 60.f;
+
+	HitVector = CreateDefaultSubobject<USceneComponent>(TEXT("HitVector"));
+	HitVector->SetupAttachment(StaticMeshComp);
+}
+void AInteractiveObstacleMover::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	CheckHit();
+}
+float AInteractiveObstacleMover::GetCurrentTime()
+{
+	float CurrentTimelineTime = (LocationSlideCurve != nullptr) ? LocationTimelineComp->GetPlaybackPosition() : RotationTimelineComp->GetPlaybackPosition();
+	return CurrentTimelineTime;
+}
+void AInteractiveObstacleMover::CheckHit()
+{
+	// If the Hit event occurs only for the desired time
+	if (bLimitedTimeHit)
+	{
+		float CurrentTimelineTime = GetCurrentTime();
+		if (CurrentTimelineTime < MinHitTime || CurrentTimelineTime > MaxHitTime) return;
+	}
+
+	// Find objects within the sphere
+	TArray<FOverlapResult> Overlaps;
+	FCollisionObjectQueryParams CollisionObjectQueryParams;
+	FCollisionQueryParams Params(SCENE_QUERY_STAT(Gimmick), false, this);
+	Params.AddIgnoredActor(this);
+
+	// Check Hited
+	const FVector OriginPos = HitVector->GetComponentLocation();
+	const FRotator OriginRot = HitVector->GetComponentRotation();
+	const FQuat& OriginQuat = StaticMeshComp->GetComponentRotation().Quaternion();
+	bool HitDetected = GetWorld()->OverlapMultiByObjectType(Overlaps, OriginPos, OriginQuat, CollisionObjectQueryParams, FCollisionShape::MakeBox(BoxCollisionExtent), Params);
+	if (HitDetected) 
+	{
+		TArray<UPrimitiveComponent*, TInlineAllocator<1>> AffectedComponents;
+		AffectedComponents.Reserve(Overlaps.Num());
+
+		for (FOverlapResult& OverlapResult : Overlaps)
+		{
+			if (UPrimitiveComponent* PrimitiveComponent = OverlapResult.Component.Get())
+			{
+				AffectedComponents.AddUnique(PrimitiveComponent);
+			}
+		}
+
+		for (UPrimitiveComponent* PrimitiveComponent : AffectedComponents)
+		{
+			// see if this is a target for a movement component
+			AActor* ComponentOwner = PrimitiveComponent->GetOwner();
+			if (ComponentOwner)
+			{
+				TScriptInterface<class ICharacterInterActionHandler> ActionHandler = ComponentOwner;
+				if (ActionHandler.GetInterface()) {
+					FVector ImpulseDirection = OriginRot.Vector() * ForceStrength;
+					ActionHandler->KnockBackActor(ImpulseDirection);
+				}
+			}
+		}
+	}
+
+	// For log
+	FColor DrawColor = HitDetected ? FColor::Green : FColor::Red;
+	DrawDebugBox(GetWorld(), OriginPos, BoxCollisionExtent, OriginQuat, DrawColor, false, 0.3f);
+}
---
+++ Source/PullAndPush/Private/Gimmick/ObstacleMover.cpp
@@ -0,0 +1,111 @@
+#include "Gimmick/ObstacleMover.h"
+#include "Net/UnrealNetwork.h"
+
+AObstacleMover::AObstacleMover()
+	:
+	MovementStartTime(0.f)
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+	SetReplicateMovement(false); 
+	NetUpdateFrequency = 60.f;
+
+	DefaultSceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneComp"));
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComp"));
+	LocationTimelineComp = CreateDefaultSubobject<UTimelineComponent>(TEXT("LocationTimelineComp"));
+	RotationTimelineComp = CreateDefaultSubobject<UTimelineComponent>(TEXT("RotationTimelineComp"));
+
+	SetRootComponent(DefaultSceneComp);
+	StaticMeshComp->SetupAttachment(GetRootComponent());
+	StaticMeshComp->SetGenerateOverlapEvents(false);
+	StaticMeshComp->SetCollisionProfileName(MeshCollisionName);
+	StaticMeshComp->SetIsReplicated(true);
+}
+void AObstacleMover::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	SetInitTimeline();
+}
+void AObstacleMover::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	InterpolateLocation(DeltaTime);
+	InterpolateRotator(DeltaTime);
+}
+void AObstacleMover::SetInitTimeline()
+{
+	if (!LocationTimelineComp || !RotationTimelineComp) return;
+
+	// Set Location & Rotation Slide
+	if (LocationSlideCurve)
+	{
+		SlideLocationInterpFunction.BindDynamic(this, &AObstacleMover::ServerUpdateLocation);
+		LocationTimelineComp->AddInterpVector(LocationSlideCurve, SlideLocationInterpFunction);
+		LocationTimelineComp->SetLooping(true);
+	}
+	if (RotationSlideCurve)
+	{
+		SlideRotationInterpFunction.BindDynamic(this, &AObstacleMover::ServerUpdateRotation);
+		RotationTimelineComp->AddInterpVector(RotationSlideCurve, SlideRotationInterpFunction);
+		RotationTimelineComp->SetLooping(true);
+	}
+
+	// Set Movement Time
+	if (GetWorld()->IsServer())
+	{
+		if (MovementStartTime < 0.1f)
+		{
+			StartMovement();
+		}
+		else
+		{
+			FTimerHandle TimelineHanlder;
+			GetWorld()->GetTimerManager().SetTimer(TimelineHanlder, this, &AObstacleMover::StartMovement, MovementStartTime, false);
+		}
+	}
+}
+void AObstacleMover::InterpolateLocation(float DeltaTime)
+{
+	// interpolation of moving the actor to the predicted position
+	if (LocationSlideCurve != nullptr)
+	{
+		FVector NewLocation = StaticMeshComp->GetRelativeLocation();
+		NewLocation = FMath::VInterpTo(NewLocation, PredictedLocation, DeltaTime, InterpolationSpeed);
+		StaticMeshComp->SetRelativeLocation(NewLocation);
+	}
+}
+void AObstacleMover::InterpolateRotator(float DeltaTime)
+{
+	if (RotationSlideCurve != nullptr)
+	{
+		FQuat CurrentRotationQuat = StaticMeshComp->GetRelativeRotation().Quaternion();
+		FQuat TargetRotationQuat = PredictedRotation.Quaternion();
+
+		FQuat NewRotationQuat = FQuat::Slerp(CurrentRotationQuat, TargetRotationQuat, DeltaTime * InterpolationSpeed);
+		FRotator NewRotation = NewRotationQuat.Rotator();
+
+		StaticMeshComp->SetRelativeRotation(NewRotation);
+	}
+}
+void AObstacleMover::ServerUpdateLocation_Implementation(const FVector NewLocation)
+{
+	PredictedLocation = NewLocation;
+}
+void AObstacleMover::ServerUpdateRotation_Implementation(const FVector NewRotator)
+{
+	PredictedRotation = FRotator(NewRotator.X, NewRotator.Y, NewRotator.Z);
+}
+void AObstacleMover::StartMovement()
+{
+	if(LocationSlideCurve != nullptr) LocationTimelineComp->Play();
+	if(RotationSlideCurve != nullptr) RotationTimelineComp->Play();
+}
+void AObstacleMover::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION(AObstacleMover, PredictedLocation, COND_SkipOwner);
+	DOREPLIFETIME_CONDITION(AObstacleMover, PredictedRotation, COND_SkipOwner);
+}
